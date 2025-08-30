040f4bd1c6beb50137f0a933cfe8aa2847698d39
Lucas Kenji <124621971+dinozindev@users.noreply.github.com>
2025-08-29 22:41:47 -0300

    Delete Sprint1-API directory

--- Sprint1-API/Dtos/MovimentacaoPostDto.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Sprint1_API.Dtos;
-
-public record MovimentacaoPostDto(
-    string DescricaoMovimentacao,
-    int MotoId,
-    int VagaId);
--- Sprint1-API/Dtos/VagasSetorDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Sprint1_API.Dtos;
-
-public record VagasSetorDto(
-    string Setor,
-    int TotalVagas,
-    int MotosPresentes
-    );

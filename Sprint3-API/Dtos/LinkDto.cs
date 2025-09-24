5d8f2ee8c43cbe0d1d8108eb8339ca931ace73aa
dinozin <dinozindev@gmail.com>
2025-09-24 12:01:41 -0300

    feat: adicionando documentação para LinkDto, PageResponse e ResourceResponse

--- Sprint3-API/Dtos/LinkDto.cs
+++ Sprint3-API/Dtos/LinkDto.cs
@@ -1,7 +1,13 @@
-﻿namespace Sprint3_API.Dtos;
+﻿using System.ComponentModel;
 
+namespace Sprint3_API.Dtos;
+
+[Description("Representa um Link HATEOAS")]
 public record LinkDto(
+    [property: Description("Relação do link (ex: self, list, next, prev...)")]
     string Rel,
+    [property: Description("URL do recurso")]
     string Href,
+    [property: Description("Método HTTP associado ao link (GET, POST, PUT, DELETE)")]
     string Method
 );
